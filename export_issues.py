39a0aac0db180b619c9ef95e62893bc485fa0044
fangzheng <fz0000@sina.com>
2022-05-25 13:23:42 +0800

    add filter
    add filter for milestone, labels, assignees,

--- export_issues.py
+++ export_issues.py
@@ -14,6 +14,11 @@ if platform.python_version_tuple()[0] == '2':
     sys.exit(0)
 
 
+class LOGIC:
+    AND = 0
+    OR = 1
+
+
 def countdown(sec):
     for i in range(sec):
         time.sleep(1)
@@ -40,44 +45,77 @@ def check_remaining():
         countdown(wait_time)
 
 
-def get_all_issues():
+def check_filter(filter_list: list = None, all_list: list = None, logic: int = LOGIC.AND):
+    if not filter_list:
+        return True  # speed up: always True if filter_list is None
+    if logic == LOGIC.AND:
+        return filter_list <= all_list
+    if logic == LOGIC.OR:
+        return [i for i in filter_list if i in all_list] > []
+
+
+def get_all_issues(state='all', milestone: str = None,
+                   labels: list[str] = None, labels_logic=LOGIC.AND,
+                   assignees: list[str] = None, assignees_logic=LOGIC.AND):
     issue_list = []
-    issues = repo.get_issues(state='all')
+    issues = repo.get_issues(state=state)
     with open('issues.csv', 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
         # todo: use friendly col name?
         writer.writerow(['id', 'number', 'title', 'labels', 'milestone', 'state', 'assignees', 'closed_at',
                          'created_at', 'last_modified', 'updated_at'])
-        total = issues.totalCount
+        check_remaining()
+        total = issues.totalCount  # remain -1
         cnt = 0
         print('Writing to issues.csv.....')
-        for issue in issues:
+        for issue in issues:  # remain -1
             cnt += 1
+
+            # if issue.pull_request: # remain -1
+            #     print('skipping PR %d\t %d/%d' % (issue.number, cnt, total))
+            #     print('check pr yes')
+            #     check_remaining()
+            #     continue
+            # print('check pr no')
+
+            # label
             tmp_label = []
             tmp_an = []
             for l in issue.labels:
                 tmp_label.append(l.name)
+            if not check_filter(labels, tmp_label, labels_logic):
+                continue
+
+            # assignees
             for an in issue.assignees:
                 if an:
                     # tmp_an.append('%s<%s>' % (an.login, (an.name or an.login)))
                     tmp_an.append(an.login)
                 else:
                     tmp_an.append('')
+            if not check_filter(assignees, tmp_an, assignees_logic):
+                continue
+
             # c_b = ''  # bad var name, but I'm too lazy...
-            i_m = ''
-            # if issue.closed_by:
+            # if issue.closed_by: # remain -1
             #     # c_b = '%s<%s>' % (issue.closed_by.login, (issue.closed_by.name or issue.closed_by.login))
             #     c_b = issue.closed_by.login
+
+            # milestone
+            tmp_milestone = ''
             if issue.milestone:
-                i_m = issue.milestone.title
-            # if cnt % 20 == 0:  # todo: check remaining every 20 issues. need customization？
+                tmp_milestone = issue.milestone.title
+            if milestone and not milestone == tmp_milestone:
+                continue
+            # if cnt % 20 == 0:  # check remaining every 20 issues.
             #     check_remaining()
+
             print('getting issue %d\t %d/%d' % (issue.number, cnt, total))
             line = [issue.id,
                     issue.number,
                     issue.title,
                     ','.join(tmp_label),
-                    i_m,
+                    tmp_milestone,
                     issue.state,
                     ','.join(tmp_an),
                     issue.closed_at,
